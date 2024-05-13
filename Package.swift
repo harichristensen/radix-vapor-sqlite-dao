902b6208ea4a52209a46debaebc8ae50b66f7693
harichristensen <harilfc@gmail.com>
2024-05-13 04:29:37 -0600

    pre radix implementation

--- Package.swift
+++ Package.swift
@@ -4,13 +4,14 @@ import PackageDescription
 let package = Package(
     name: "radix-vapor-sqlite-dao",
     platforms: [
-       .macOS(.v13)
+       .macOS(.v13),
+       .iOS(.v11)
     ],
     dependencies: [
         // openapi packages
-        // .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
-        // .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
-        // .package(url: "https://github.com/swift-server/swift-openapi-vapor", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
+        .package(url: "https://github.com/swift-server/swift-openapi-vapor", from: "1.0.0"),
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.92.4"),
         // 🗄 An ORM for SQL and NoSQL databases.
@@ -19,23 +20,24 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.6.0"),
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "4.3.0"),
-        
+        .package(url: "https://github.com/radixdlt/swift-engine-toolkit", exact: "1.0.0")
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
-                // .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                // .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 .product(name: "Leaf", package: "leaf"),
                 .product(name: "Vapor", package: "vapor"),
+                .product(name: "EngineToolkit", package: "swift-engine-toolkit")
             ],
-            swiftSettings: swiftSettings
-            // plugins: [
-            //     .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
-            // ]
+            swiftSettings: swiftSettings,
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+            ]
         ),
         .testTarget(
             name: "AppTests",
